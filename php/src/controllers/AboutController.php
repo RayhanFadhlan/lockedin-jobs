fb5964d2e783927bd0e57a3073036046e8ee0bb7
Rayhan Fadhlan <rayhanfazka07@gmail.com>
2024-10-09 21:43:03 +0700

    feat : routing setup

---
+++ php/src/controllers/AboutController.php
@@ -0,0 +1,7 @@
+<?php
+
+class AboutController {
+    public function index() {
+        include __DIR__ . '/../views/about.php';
+    }
+}
--- php/src/index.php
+++ php/src/index.php
@@ -1,4 +1,28 @@
 <?php
 
-echo 'Hello World!';
-echo '<script src="/public/index.js"></script>';
+require_once __DIR__ . '/routes.php';
+
+function route($uri, $routes) {
+    if (array_key_exists($uri, $routes)) {
+        $controller = $routes[$uri][0];
+        $method = $routes[$uri][1];
+        
+        require_once __DIR__ . "/controllers/{$controller}.php";
+        
+        $controllerInstance = new $controller();
+        $controllerInstance->$method();
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo "404 Not Found";
+    }
+}
+
+$uri = $_SERVER['REQUEST_URI'];
+$uri = strtok($uri, '?');
+$uri = rtrim($uri, '/');
+
+if (empty($uri)) {
+    $uri = '/';
+}
+
+route($uri, $routes);
