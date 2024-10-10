bacc137ed7ad6099d163dd44bbc92f885af1a2a8
William Glory <williamgloryh@gmail.com>
2024-10-10 15:45:11 +0700

    fix: fix database using env

--- php/src/config/database.php
+++ php/src/config/database.php
@@ -1,9 +1,18 @@
 <?php
 
-return [
-    'host' => 'localhost',
-    'port' => '5432',
-    'database' => 'testing',
-    'username' => 'postgres',
-    'password' => 'root'
-];
+// return [
+//     'host' => 'localhost',
+//     'port' => '5432',
+//     'database' => 'testing',
+//     'username' => 'postgres',
+//     'password' => 'root'
+// ];
+
+use setup\EnvLoader;
+
+(new EnvLoader(__DIR__.'/.env'))->load();
+define('host', getenv('DB_HOST'));
+define('port', getenv('DB_PORT'));
+define('database', getenv('DB_NAME'));
+define('username', getenv('DB_USER'));
+define('password', getenv('DB_PASSWORD'));
