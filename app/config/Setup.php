8e8502f95ac7ea29379cd8fe269ce87e824f04bc
Rayhan Fadhlan <rayhanfazka07@gmail.com>
2024-10-11 21:57:38 +0700

    fix : env docker

--- app/config/Setup.php
+++ app/config/Setup.php
@@ -2,14 +2,36 @@
 
 use setup\EnvLoader;
 
-$envPath = isset($_ENV['DOCKER_ENV']) ? '/var/www/html/.env' : __DIR__ . '/../../.env';
-$dbHost = isset($_ENV['DOCKER_ENV']) ? getenv('DB_HOST') : '127.0.0.1';
-(new EnvLoader($envPath))->load();
+// $envPath = isset($_ENV['DOCKER_ENV']) ? '/var/www/html/.env' : __DIR__ . '/../../.env';
+// $dbHost = isset($_ENV['DOCKER_ENV']) ? getenv('DB_HOST') : '127.0.0.1';
+// (new EnvLoader($envPath))->load();
 
-return [
-    'host' => $dbHost,
-    'port' => getenv('DB_PORT'),
-    'name' => getenv('DB_NAME'),
-    'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASSWORD'),
-];
+// return [
+//     'host' => $dbHost,
+//     'port' => getenv('DB_PORT'),
+//     'name' => getenv('DB_NAME'),
+//     'username' => getenv('DB_USER'),
+//     'password' => getenv('DB_PASSWORD'),
+// ];
+
+if(isset($_ENV['DOCKER_ENV'])) {
+    return [
+        'host' => getenv('DB_HOST'),
+        'port' => getenv('DB_PORT'),
+        'name' => getenv('DB_NAME'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+    ];
+   
+}else {
+
+    $envPath = __DIR__ . '/../../.env';
+    (new EnvLoader($envPath))->load();
+    return [
+        'host' => 'localhost',
+        'port' => getenv('DB_PORT'),
+        'name' => getenv('DB_NAME'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASSWORD'),
+    ];
+}
