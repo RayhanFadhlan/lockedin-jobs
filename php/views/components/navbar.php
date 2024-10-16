32ff2db565edf04ae99356e440810cca3998c05a
William Glory <williamgloryh@gmail.com>
2024-10-16 17:52:34 +0700

    feat: navbar custom

---
+++ php/views/components/navbar.php
@@ -0,0 +1,62 @@
+<nav class="navbar">
+    <div class="navbar-left">
+        <span class="logo"> <img src="../../public/images/logo.png"></span>
+    </div>
+    <div class="navbar-right">
+        <a href="/" class="nav-item">
+            <span class="nav-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house">
+                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+                </svg>
+            </span>
+            Home
+        </a>
+        <?php if (isset($_SESSION['user']['role'])): ?>
+            <?php if ($_SESSION['user']['role'] === 'jobseeker'): ?>
+                <a href="/history" class="nav-item"> 
+                    <span class="nav-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-history">
+                            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
+                            <path d="M3 3v5h5"/>
+                            <path d="M12 7v5l4 2"/>
+                        </svg>
+                    </span>
+                    History
+                </a>
+            <?php elseif ($_SESSION['user']['role'] === 'company'): ?>
+                <a href="/profile" class="nav-item"> 
+                    <span class="nav-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-plus">
+                            <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+                            <circle cx="8.5" cy="7" r="4"/>
+                            <path d="M20 8v6M23 11h-6"/>
+                        </svg>
+                    </span>
+                    Sign Up
+                </a>
+            <?php endif; ?>
+        <?php endif; ?>
+        
+        <?php if (isset($_SESSION['user']['role'])): ?>
+            <?php if ($_SESSION['user']['role'] === 'jobseeker' || $_SESSION['user']['role'] === 'company'): ?>
+                <div>
+                    <a href="#" class="nav-link" id="profileDropdown">
+                        <span class="nav-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="10" r="3"/>
+                            <path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"/>
+                        </svg>
+                    </span>
+                        Me
+                    </a>
+                    <div class="dropdown-menu" id="profileDropdownMenu">
+                        <a href="/profile" class="dropdown-item">View Profile</a>
+                        <a href="/signup" class="dropdown-item">Sign Out</a>
+                    </div>
+                </div>
+            <?php else: ?>
+                <button>
+            <?php endif; ?>
+        <?php endif; ?>
+    </div>
+</nav>
--- php/views/home.php
+++ php/views/home.php
@@ -12,36 +12,37 @@
             </select>
         </div>
     </div>
-
-    <div class="left-content">
-        <div class="filter-by">
-            <div class="dropdown">
-                <button class="dropdown-btn" data-dropdown-id="jobType">Job Type</button>
-                <div class="dropdown-content">
-                    <label><input type="checkbox" value="Full-time"> Full Time</label>
-                    <label><input type="checkbox" value="Part-time"> Part Time</label>
-                    <label><input type="checkbox" value="internship"> Internship</label>
+    <div class="main-content">
+        <div class="left-content">
+            <div class="filter-by">
+                <div class="dropdown">
+                    <button class="dropdown-btn" data-dropdown-id="jobType">Job Type</button>
+                    <div class="dropdown-content">
+                        <label><input type="checkbox" value="Full-time"> Full Time</label>
+                        <label><input type="checkbox" value="Part-time"> Part Time</label>
+                        <label><input type="checkbox" value="internship"> Internship</label>
+                    </div>
                 </div>
-            </div>
 
-            <div class="dropdown">
-                <button class="dropdown-btn" data-dropdown-id="jobPlace">Job Place</button>
-                <div class="dropdown-content">
-                    <label><input type="checkbox" value="On-site"> On Site</label>
-                    <label><input type="checkbox" value="remote"> Remote</label>
-                    <label><input type="checkbox" value="hybrid"> Hybrid</label>
+                <div class="dropdown">
+                    <button class="dropdown-btn" data-dropdown-id="jobPlace">Job Place</button>
+                    <div class="dropdown-content">
+                        <label><input type="checkbox" value="On-site"> On Site</label>
+                        <label><input type="checkbox" value="remote"> Remote</label>
+                        <label><input type="checkbox" value="hybrid"> Hybrid</label>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="right-content"> 
+        <div class="right-content"> 
 
-        <div class="pagination">
-            <button class="pagination-button" id="prev-page" disabled>&lt;</button>
-            <div class="page-numbers">
+            <div class="pagination">
+                <button class="pagination-button" id="prev-page" disabled>&lt;</button>
+                <div class="page-numbers">
+                </div>
+                <button class="pagination-button" id="next-page">&gt;</button>
             </div>
-            <button class="pagination-button" id="next-page">&gt;</button>
         </div>
     </div>
 </div>
