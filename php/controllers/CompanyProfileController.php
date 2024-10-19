04688a5294b8da9e218fb8c7ce16f5c5c6b54291
Moses-ui <13520052@std.stei.itb.ac.id>
2024-10-19 09:59:25 +0700

    fix: change user name session

--- php/controllers/CompanyProfileController.php
+++ php/controllers/CompanyProfileController.php
@@ -50,6 +50,7 @@ class CompanyProfileController extends Controller {
             }
 
             $this->companyProfileModel->setCompanyProfile($userId, $companyName, $location, $about);
+            $_SESSION['user']['name'] = $request->getBody('company_name');
 
             Redirect::withToast('/profile', 'User created successfully');
         } catch (\Exception $e) {
--- php/views/profile-edit.php
+++ php/views/profile-edit.php
@@ -1,17 +1,17 @@
 <div class="container">
     <form action="/profile/edit" method="post" class="card">
         <label for="company-name">Company Name</label>
-        <input type="text" id="company-name" name="company_name" required>
+        <input type="text" id="company-name" name="company_name" value="<?= $name['nama'] ?>" required>
         
         <label for="company-location">Location</label>
-        <input type="text" id="company-location" name="location" required>
+        <input type="text" id="company-location" name="location" value="<?= $detail['lokasi'] ?>" required>
 
         <label for="company-about">About</label>
-        <input type="text" id="company-about" name="about" rows="3" required>
+        <input type="text" id="company-about" name="about" rows="3" value="<?= $detail['about'] ?>" required>
 
-        <button type="submit" class="primary-btn">Confirm</button>
+        <button type="button" id="cancel-btn" class="primary-btn">Cancel</button>
+        <button type="submit" class="primary-btn">Save</button>
     </form>
 </div>
-
 
 <script src="/public/scripts/profile-edit.js"></script>
