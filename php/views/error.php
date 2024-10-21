a9eb61ece5257543b14ede1b12e00768927d3bf3
William Glory <williamgloryh@gmail.com>
2024-10-21 22:57:20 +0700

    feat: add error page

---
+++ php/views/error.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="top-bar">
+        <div class="logo-error">
+            <img src="../public/images/logo.png" alt="Logo">
+        </div>
+    </div>
+    <div class="content">
+        <h1>Page not found</h1>
+        <p>Uh oh, we can't seem to find the page you're looking for. Try going back to the previous page or see our <a href="#" class="help-link">Help Center</a> for more information</p>
+        <a href="#" class="button">Go to your feed</a>
+    </div>
+</div>
