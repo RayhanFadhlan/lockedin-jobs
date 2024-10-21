df74b33675d64b355a5b6e580f3d54866c6665da
banaman <ahmadalbana98@gmail.com>
2024-10-22 01:01:24 +0700

    fix: path riwayat di navbar

--- php/views/components/navbar.php
+++ php/views/components/navbar.php
@@ -15,7 +15,7 @@
             </a>
             <?php if (isset($_SESSION['user']['role'])): ?>
                 <?php if ($_SESSION['user']['role'] === 'jobseeker'): ?>
-                    <a href="/history" class="nav-item"> 
+                    <a href="/lamaran/riwayat" class="nav-item"> 
                         <span class="nav-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-history">
                                 <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
